12e958a4e4b3808e7fd9c30d4d80c413a895745d
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-05-05 20:31:49 +0300

    add autoload

---
+++ components/Autoload.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Админ
+ * Date: 05.05.2018
+ * Time: 20:27
+ */
+
+/**
+ * Функция __autoload для автоматического подключения классов
+ */
+function __autoload($class_name){
+
+// Массив папок, в которых могут находиться необходимые классы
+    $array_paths = array(
+        '/models/',
+        '/components/',
+        '/controllers/',
+    );
+    // Проходим по массиву папок
+    foreach ($array_paths as $path) {
+        // Формируем имя и путь к файлу с классом
+        $path = ROOT . $path . $class_name . '.php';
+        // Если такой файл существует, подключаем его
+        if (is_file($path)) {
+            include_once $path;
+        }
+    }
+}
